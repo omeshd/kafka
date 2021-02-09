7802eb76552e107e42131555b974e405a80f3948
omesh-dhanushka <omesh.d@aeturnum.com>
2021-02-10 01:32:41 +0530

    add update user logs

--- src/main/java/com/example/kafka/application/UserManagementApplicationServiceImpl.java
+++ src/main/java/com/example/kafka/application/UserManagementApplicationServiceImpl.java
@@ -50,6 +50,10 @@ public class UserManagementApplicationServiceImpl implements UserManagementAppli
     @Override
     public void updateUser(UpdateUserRQ updateUserRQ, String userId) {
 
+        Map<String, String> logMap = new HashMap<String, String>();
+        logMap.put(LoggingEventField.USERNAME, updateUserRQ.getFirstName());
+        logMap.put(LoggingEventField.PHONE, updateUserRQ.getLastName());
+        LoggingUtil.logInfo(logger, "Create user start.", logMap, LoggingEvent.CREATE_USER);
     }
 
     @Override
